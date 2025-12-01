3e5681df7f533168b62b84561a75855a889d621c
Ulriche-Dev <ulriche.m@h2altitude>
2025-12-01 13:11:37 +0100

    Modif: Mise à jour du texte sur la page d'accueil

--- accueil/views.py
+++ accueil/views.py
@@ -3,22 +3,37 @@ from django.shortcuts import render
 # from django.http import HttpResponse
 
 from django.shortcuts import render
-from datetime import date
+from datetime import date, datetime
 from .forms import NomForm
 
 def bonjour(request):
-    message = "Bonjour, le monde de Django !"
+    current_hour = datetime.now().hour
+    if current_hour < 12:
+        message = "Bon matin !"
+    else:
+        message = "Bon après-midi !"
+
     if request.method == 'POST':
         form = NomForm(request.POST)
         if form.is_valid():
             nom = form.cleaned_data['nom']
-            message = f"Bonjour, {nom} !"
+            message = f"{message} {nom} !"
     else:
         form = NomForm()
+
+    # Liste de tâches
+    taches = [
+        "Apprendre Django",
+        "Créer un projet web",
+        "Faire une pause café",
+        "Boire un café",
+        "Déployer l'application"
+    ]
 
     context = {
         'message': message,
         'date_du_jour': date.today(),
-        'form': form
+        'form': form,
+        'taches': taches
     }
     return render(request, 'accueil/index.html', context)
