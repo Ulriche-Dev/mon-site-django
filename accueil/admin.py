8a238e1c0971714e28c9a1bfa573745aa8179421
Ulriche-Dev <ulriche.m@h2altitude>
2025-12-01 14:30:57 +0100

    Feat: Ajout du style CSS pour la liste des tâches

--- accueil/admin.py
+++ accueil/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Tache
 
-# Register your models here.
+@admin.register(Tache)
+class TacheAdmin(admin.ModelAdmin):
+    list_display = ('titre', 'termine')  # Affiche ces champs dans la liste des tâches
+    list_filter = ('termine',)          # Ajoute un filtre pour le champ 'termine'
+    search_fields = ('titre',)          # Ajoute une barre de recherche pour le champ 'titre'
--- accueil/models.py
+++ accueil/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Tache(models.Model):
+    titre = models.CharField(max_length=200)
+    termine = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titre
